c99877ad4a83362431ee5929733c3f8e134d6710
Chrish Dunne <hey@chrish.design>
2021-09-15 16:23:09 +0100

    feat: Added prettier and stylelint

--- packages/hw/src/Header.js
+++ packages/hw/src/Header.js
@@ -1,32 +1,30 @@
-import React from 'react'
-import PropTypes from 'prop-types';
-import { styled } from 'frontity';
+import React from "react";
+import PropTypes from "prop-types";
+import { styled } from "frontity";
 
 // ---
 
 const Header = ({ title, bio }) => (
-    <HeaderWrap>
-      <Logo>
+  <HeaderWrap>
+    <Logo>
       <h1>{title}</h1>
-      </Logo>
-      <p>{bio}</p>
-    </HeaderWrap>
-  )
+    </Logo>
+    <p>{bio}</p>
+  </HeaderWrap>
+);
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
   bio: PropTypes.string.isRequired,
-}
+};
 
-export default Header
+export default Header;
 
-const HeaderWrap = styled.header`
-  
-`
+const HeaderWrap = styled.header``;
 
 const Logo = styled.div`
-h1 {
+  h1 {
     font-weight: 800;
     text-transform: uppercase;
   }
-`
+`;
