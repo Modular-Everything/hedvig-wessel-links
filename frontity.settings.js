e7bab270948b1bb22fab9328d029dd4afb3e5036
Chrish Dunne <hey@chrish.design>
2021-09-16 14:21:41 +0100

    feat: Updated source prefix

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,27 +1,30 @@
 const settings = {
-  "name": "hedvig-wessel-links",
-  "state": {
-    "frontity": {
-      "url": "https://hedvigwessel.no",
-      "title": "Hedvig Wessel Links",
-      "description": "Linktree for Hedvig Wessel."
-    }
+  name: "hedvig-wessel-links",
+  state: {
+    frontity: {
+      url: "https://hedvigwessel.no/",
+      title: "Hedvig Wessel Links",
+      description: "Linktree for Hedvig Wessel.",
+    },
   },
-  "packages": [
+  packages: [
     {
-      name: 'hw',
+      name: "hw",
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "url": "https://hedvigwessel.no"
-        }
-      }
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          url: "https://hedvigwessel.no/",
+        },
+        wpSource: {
+          prefix: "?rest_route=/",
+        },
+      },
     },
     "@frontity/tiny-router",
-    "@frontity/html2react"
-  ]
+    "@frontity/html2react",
+  ],
 };
 
 export default settings;
