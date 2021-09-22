ae49fe6393739c573e8ef0c1034f819e051e30c2
Chrish Dunne <hey@chrish.design>
2021-09-22 15:13:45 +0100

    feat: Added footer

---
+++ packages/hw/src/components/Footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect, styled } from "frontity";
+
+const Footer = ({ footerText, libraries }) => {
+  const Html2React = libraries.html2react.Component;
+
+  return (
+    <FooterWrap>
+      <Html2React html={footerText} />
+      <p>&copy; {new Date().getFullYear()} Hedvig Wessel</p>
+    </FooterWrap>
+  );
+};
+
+Footer.propTypes = {
+  footerText: PropTypes.string.isRequired,
+  libraries: PropTypes.object.isRequired,
+};
+
+export default connect(Footer);
+
+const FooterWrap = styled.footer`
+  margin-top: 3.2rem;
+
+  p {
+    width: 80%;
+    max-width: 32rem;
+    margin: 0 auto 1.6rem auto;
+    font-family: var(--mono);
+    font-size: 1rem;
+    text-align: center;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+`;
