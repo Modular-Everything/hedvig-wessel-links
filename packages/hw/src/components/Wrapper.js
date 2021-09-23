6bda5c15dda254482b59f154c5194b08f0e2810d
Chrish Dunne <hey@chrish.design>
2021-09-23 10:21:00 +0100

    fix: Incorrect crossorigin usage

--- packages/hw/src/components/Wrapper.js
+++ packages/hw/src/components/Wrapper.js
@@ -36,7 +36,7 @@ const Wrapper = ({ libraries }) => {
   return (
     <>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link
         href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
         rel="stylesheet"
